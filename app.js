9a7e4789592bb842ef51ad22a7c35aa338b2d35b
1337-hacks <elijahnucum.exe@gmail.com>
2021-10-01 15:11:55 +1300

    c001/route and mongoose js file changes

--- app.js
+++ app.js
@@ -22,3 +22,29 @@ app.listen(port, () => {
   });
   console.log(`Server is running on port: ${port}`);
 });
+
+//-------------------------------------------------------------------------------
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const path = require('path')
+// const app = express();
+// require('./db');
+
+// app.use(bodyParser.json());
+// app.use(cors());
+
+// // API
+// const users = require('/api/users');
+// app.use('/api/users', users);
+
+// app.use(express.static(path.join(__dirname, '../build')))
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../build'))
+// })
+
+// const port = process.env.PORT || 5000;
+// app.listen(port, () => {
+//     console.log(`Server started on port ${port}`);
+// });
