1570c067d946d1022a903532672712158ebeef40
1337-hacks <elijahnucum.exe@gmail.com>
2021-10-02 21:47:16 +1300

    c002/fetching data fixed

--- app.js
+++ app.js
@@ -1,50 +1,27 @@
 // app.js
 
 const express = require("express");
-const app = express();
 const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 7000;
+const app = express();
+const path = require('path');
+require("./db/conn");
 
 app.use(cors());
 app.use(express.json());
-app.use(require("./routes/articles"));
 
-// get driver connection
-const dbo = require("./db/conn");
- 
-app.listen(port, () => {
+// API
 
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
- 
-  });
-  console.log(`Server is running on port: ${port}`);
-});
-
-//-------------------------------------------------------------------------------
-
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
-// const path = require('path')
-// const app = express();
-// require('./db');
-
-// app.use(bodyParser.json());
-// app.use(cors());
-
-// // API
-// const users = require('/api/users');
-// app.use('/api/users', users);
+const articles = require("./routes/articles");
+app.use("/routes/articles", articles);
 
 // app.use(express.static(path.join(__dirname, '../build')))
 // app.get('*', (req, res) => {
 //     res.sendFile(path.join(__dirname, '../build'))
-// })
+// }) 
 
-// const port = process.env.PORT || 5000;
-// app.listen(port, () => {
-//     console.log(`Server started on port ${port}`);
-// });
+// Start Server
+
+const port = process.env.PORT || 7000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
--- routes/articles.js
+++ routes/articles.js
@@ -1,10 +1,8 @@
 // articles.js - Middleware that will take care of requests for the database.
 
 const express = require('express');
-
 const router = express.Router();
 
-// Load Article model
 const Article = require('../models/articleModel');
 
 // Route testing
@@ -13,8 +11,9 @@ router.get('/test', (req, res) => res.send('Article route testing!'));
 // Get all articles
 router.get('/', (req, res) => {
   Article.find()
-    .then(articles => res.json(articles))
+    .then(data => res.json(data))
     .catch(err => console.log(err));
 });
+
 
 module.exports = router;
