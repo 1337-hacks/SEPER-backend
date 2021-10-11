0d9998c5848565867f087da8a5fb3e90c0d16696
cieloren <roquid.1217@outlook.com>
2021-10-11 20:20:41 +1300

    Commit 003/ren trying heroku

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// app.js
+app.js
 
 const express = require("express");
 const app = express();
@@ -22,3 +22,37 @@ app.listen(port, () => {
   });
   console.log(`Server is running on port: ${port}`);
 });
+
+// ren try XD
+
+// import npm packages
+
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const morgan = require('morgan');
+// const path = require('path');
+
+// const app = express();
+// const port = process.env.PORT || 8080;
+
+// const routes = require('./routes/articles');
+
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://elijah2:<password>@seper-db.f19fc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
+
+// mongoose.connection.on('connected', () => {
+//   console.log('mongoose is connected yessir!!!!')
+// });
+
+// // data parsing
+// app.use(express.json());
+// app.use(express.urlencoded({extended: false}));
+
+// app.use(morgan('tiny'));
+// app.use('/api', routes);
+
+// if (process === 'production') {
+
+// }
